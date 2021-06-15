ccab91fa5b3803a9ce1bb358c67380707e893c86
Cory Bass <cory.bass.07@gmail.com>
2021-06-15 18:04:38 -0500

    testing classes complete, all functional

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,21 @@
-import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 
 // const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-// const sorter = new Sorter(numbersCollection);
-// sorter.sort();
+// numbersCollection.sort();
 // console.log(numbersCollection.data);
 
 // const charactersCollection = new CharactersCollection('XakdfjFGk');
-// const sorter = new Sorter(charactersCollection);
-// sorter.sort();
+// charactersCollection.sort();
 // console.log(charactersCollection.data);
 
-// const linkedList = new LinkedList();
-// linkedList.add(500);
-// linkedList.add(-432);
-// linkedList.add(234);
-// linkedList.add(23);
-// linkedList.add(5);
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-432);
+linkedList.add(234);
+linkedList.add(23);
+linkedList.add(5);
 
-// const sorter = new Sorter(linkedList);
-// sorter.sort();
-// console.log(linkedList.print());
+linkedList.sort();
+console.log(linkedList.print());
